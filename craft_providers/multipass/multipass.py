a6b3f9941220a53ab4df32bbcd9d042fc9cc636a
Chris Patterson <chris.patterson@canonical.com>
2021-03-17 08:11:04 -0400

    osx

--- craft_providers/multipass/multipass.py
+++ craft_providers/multipass/multipass.py
@@ -11,6 +11,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """API provider for Multipass.
 
 This implementation interfaces with multipass using the `multipass` command-line
@@ -26,7 +27,6 @@ import subprocess
 from typing import Any, Dict, List, Optional
 
 from craft_providers import errors
-
 from .errors import MultipassError
 
 logger = logging.getLogger(__name__)
