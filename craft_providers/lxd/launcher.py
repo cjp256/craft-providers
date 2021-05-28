1d4c3dbae6c9d74e32103dfd8aa540d78b7e90e1
Chris Patterson <chris.patterson@canonical.com>
2021-05-28 09:59:29 -0400

    lxd: add snapshot support
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- craft_providers/lxd/launcher.py
+++ craft_providers/lxd/launcher.py
@@ -18,9 +18,45 @@ import logging
 
 from craft_providers import Base, bases
 
+from .lxc import LXC
 from .lxd_instance import LXDInstance
 
 logger = logging.getLogger(__name__)
+
+
+def _formulate_snapshot_image_name(
+    *, image_name: str, image_remote: str, compatibility_tag: str
+) -> str:
+    return "-".join(
+        [
+            "snapshot",
+            image_remote,
+            image_name,
+            compatibility_tag,
+        ]
+    )
+
+
+def _publish_snapshot(
+    *,
+    lxc: LXC,
+    snapshot_name: str,
+    instance: LXDInstance,
+    base_configuration: Base,
+) -> None:
+    """Publish snapshot from instance."""
+    # Instance must be stopped for proper publishing.
+    instance.stop()
+
+    lxc.publish(
+        alias=snapshot_name,
+        instance_name=instance.name,
+        force=True,
+    )
+
+    # Restart container and ensure it is ready.
+    instance.start()
+    base_configuration.wait_until_ready(executor=instance)
 
 
 def launch(
@@ -32,6 +68,8 @@ def launch(
     auto_clean: bool = False,
     ephemeral: bool = False,
     map_user_uid: bool = False,
+    use_snapshots: bool = False,
+    lxc: LXC = LXC(),
 ) -> LXDInstance:
     """Create, start, and configure instance.
 
@@ -45,6 +83,7 @@ def launch(
     :param auto_clean: Automatically clean instance, if incompatible.
     :param ephemeral: Create ephemeral instance.
     :param map_user_uid: Map current uid/gid to instance's root uid/gid.
+    :param use_snapshots: Use LXD snapshots for bootstrapping images.
 
     :returns: LXD instance.
 
@@ -72,6 +111,20 @@ def launch(
             else:
                 raise
 
+    # Create from snapshot, if available.
+    snapshot_name = _formulate_snapshot_image_name(
+        image_name=image_name,
+        image_remote=image_remote,
+        compatibility_tag=base_configuration.compatibility_tag,
+    )
+    if use_snapshots and lxc.has_image(image_name=snapshot_name):
+        logger.debug("Using compatible snapshot %r.", snapshot_name)
+        image_name = snapshot_name
+        image_remote = "local"
+
+        # Don't re-publish this snapshot later.
+        use_snapshots = False
+
     instance.launch(
         image=image_name,
         image_remote=image_remote,
@@ -79,4 +132,18 @@ def launch(
         map_user_uid=map_user_uid,
     )
     base_configuration.setup(executor=instance)
+
+    # Publish snapshot if enabled and instance is not ephemeral.
+    if use_snapshots:
+        if ephemeral:
+            logger.debug("Refusing to publish snapshot for ephemeral instance.")
+        else:
+            logger.debug("Publishing snapshot from instance %r.", snapshot_name)
+            _publish_snapshot(
+                lxc=lxc,
+                snapshot_name=snapshot_name,
+                instance=instance,
+                base_configuration=base_configuration,
+            )
+
     return instance
